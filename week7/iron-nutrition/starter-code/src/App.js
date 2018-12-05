5bc62a0169f7ed86c71be670de7d0975edcac653
nickborbe <nickborbe@gmail.com>
2018-12-05 11:47:23 -0500

    iron nutrition added

---
+++ week7/iron-nutrition/starter-code/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Main from './Main'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Main />
+    
+      </div>
+    );
+  }
+}
+
+export default App;
